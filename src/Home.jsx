5e0414a55379dd93a7fa4fa15fa1f0c881cfa3c5
Camila <camilappadalino@gmail.com>
2023-09-11 16:05:48 -0300

    adicionando o codigo

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,11 @@ function Home() {
 
   return (
     <>
-      <h1>Home</h1>
-      <p></p>
+      <h1>Jóias Celestiais</h1>
+      <p>
+      Lorem ipsum vulputate ac nullam sed dictumst molestie sagittis litora sociosqu morbi sit luctus, orci accumsan primis gravida tempor maecenas lorem tincidunt eget ut fringilla. erat sollicitudin venenatis vestibulum nisl amet et non, ac porttitor vitae hendrerit duis aenean congue, cubilia nulla risus ultricies torquent magna. interdum litora integer conubia aliquam vehicula elit torquent, cursus et tempus sit tincidunt eu tellus, eros dapibus praesent rutrum feugiat suspendisse. nibh in vivamus duis rutrum molestie ligula mattis, tincidunt molestie potenti donec et cras. elementum fusce facilisis posuere in cubilia magna erat curabitur dictumst consectetur duis, orci nibh facilisis ad donec id nisl lectus blandit pellentesque taciti, purus dolor pulvinar molestie sapien etiam massa eleifend cras risus. 
+      Ullamcorper sagittis libero vehicula aliquet vehicula porttitor mollis convallis, gravida rutrum etiam ante augue cubilia fermentum, a ad iaculis nunc aliquet nam rutrum. convallis posuere convallis purus lobortis inceptos nisl praesent pulvinar sociosqu quam, fringilla aenean erat ligula lectus vehicula feugiat non fames, malesuada class sodales tortor nam ut convallis sed vestibulum. nullam dapibus ut magna fringilla ante libero egestas, sed netus lectus aliquam mattis ut turpis, cras ornare iaculis euismod dictum pretium. risus netus sagittis eu laoreet fringilla hac consequat ipsum adipiscing, augue viverra fermentum morbi netus at nisi faucibus, habitasse convallis scelerisque arcu donec vitae nisl curabitur iaculis, donec integer torquent curae aenean fames vehicula.
+      </p>
     </>
   )
 }
