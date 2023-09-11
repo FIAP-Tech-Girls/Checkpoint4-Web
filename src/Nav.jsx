ddce7f54def33051ffcb6e8821819b0d3ae4750e
letyresina <leticiaresina@gmail.com>
2023-09-11 15:52:30 -0300

    Subindo nav e rotas (Não testado)

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,15 @@
-import { } from 'react'
+import {} from 'react'
+import {Link} from 'react-router-dom'
+import '../Componentes/App.css'
 
-
-function Nav() {
-  
-
+function Nav(){
   return (
     <>
-      
+    <nav className="menu">
+      <Link to="/" id="tLink">Home</Link>
+      <Link to="Produtos" id="tLink">Produtos</Link>
+      <Link to="Sobre" id="tLink">Sobre</Link>
+    </nav>
     </>
   )
 }
