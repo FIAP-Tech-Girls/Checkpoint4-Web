847f4e469d736aabd40f75d76cc20f9d98b53526
Camila <camilappadalino@gmail.com>
2023-09-11 16:42:23 -0300

    adicionando produtos

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,12 +1,21 @@
 import { } from 'react'
-
+import Brinco from './assets/brinco.jpg'
+import Colar from './assets/colar.jpg'
 
 function Produtos() {
   
 
   return (
     <>
-      
+      <div class="joias">
+
+        <h2>Brinco</h2>
+        <img src= {Brinco} alt="brinco" />
+
+        <h2>Colar</h2>
+        <img src={Colar} alt="colar" />
+        
+      </div>
     </>
   )
 }
