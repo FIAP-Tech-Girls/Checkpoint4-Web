b47c04e1dbda9b297fc98188321f285942d7b41e
anny-dias <annydiascmd@gmail.com>
2023-09-11 16:11:05 -0300

    Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,10 @@ function Footer() {
 
   return (
     <>
-      
+      <h2>Jóias Celestiais</h2>
+      <h2>Contato</h2>
+      <p>Telefone: (11) 2594-3555</p>
+      <p>E-mail: atendimento@joiascelestiais.com.br</p>
     </>
   )
 }
